c54dcb8ae8977f278a633a8345bf3682eac2a86f
mouadh.hsoumi <mouadh.hsoumi@hstech.tn>
2021-10-03 19:44:03 +0200

    feat: added type generics support

---
+++ generic.test.ts
@@ -0,0 +1,26 @@
+import { assertEquals } from "testing/asserts.ts";
+import axiod from "./mod.ts";
+
+Deno.test("Axiod using generic type", async () => {
+  const { data } = await axiod<{ delay: string }>(
+    "https://postman-echo.com/delay/2"
+  );
+
+  assertEquals(data.delay, "2");
+});
+
+Deno.test("Axiod.request using generic type", async () => {
+  const { data } = await axiod.request<{ delay: string }>({
+    url: "https://postman-echo.com/delay/2",
+  });
+
+  assertEquals(data.delay, "2");
+});
+
+Deno.test("Axiod.get using generic type", async () => {
+  const { data } = await axiod.get<{ delay: string }>(
+    "https://postman-echo.com/delay/2"
+  );
+
+  assertEquals(data.delay, "2");
+});
