ae196d63bcdaf7b9cff072c08ec40fb370871c71
Maximilian Schmidt <maximilian5189@posteo.de>
2021-12-20 19:00:48 +0100

    redirect testcases

---
+++ redirect.test.ts
@@ -0,0 +1,28 @@
+import { assertEquals } from "./mods/testing-assert.ts";
+import axiod from "./mod.ts";
+import { TestServer } from "./mods/test-server.ts"
+
+const port = 8080
+
+Deno.test("Axiod does not follow redirect", async () => {
+  const server = new TestServer(port);
+
+  const res = await axiod.get(
+    `http://localhost:${port}/redirect`,
+    { redirect: "manual" }
+  );
+
+  assertEquals(res.status, 301);
+  server.abort();
+});
+
+Deno.test("Axiod follows redirect", async () => {
+  const server = new TestServer(port);
+
+  const res = await axiod.get(
+    `http://localhost:${port}/redirect`,
+  );
+
+  assertEquals(res.status, 200);
+  server.abort();
+});
